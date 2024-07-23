1c665b3ee7c2555fa689bb1eb5448b5760134d08
easydigs <easydigitalkoncept@gmail.com>
2024-07-23 12:54:46 +0100

    Task update

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,10 @@
 export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this;
+    const self = this;
     this.addNeighborhood = (newNeighborhood) => {
-    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+        self.sanFranciscoNeighborhoods.push(newNeighborhood);
 
-    return self.sanFranciscoNeighborhoods;
-  };
+        return self.sanFranciscoNeighborhoods;
+    };
 }
