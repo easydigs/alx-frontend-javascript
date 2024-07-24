3683e6a8949a1c688bcb0be98ad6693b5ebdab80
easydigs <easydigitalkoncept@gmail.com>
2024-07-24 21:11:14 +0100

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,10 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    const response = {
-      status: 200,
-      body: 'Success',
-    };
-
     if (success) {
-      resolve(response);
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
