004a48a3812f974262876ecc9089e773507c1d48
easydigs <easydigitalkoncept@gmail.com>
2024-07-24 19:37:04 +0100

    Keep every promise you make

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,12 @@
 export default function getResponseFromAPI() {
-  const success = true;
   return new Promise((resolve, reject) => {
-    if (success) {
-      resolve();
-    } else {
-      reject();
-    }
+    setTimeout(() => {
+      const success = true;
+      if (success) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 2000);
   });
 }
