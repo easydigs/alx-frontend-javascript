c66785862bac17768f29fabf659598a2be887f3c
easydigs <easydigitalkoncept@gmail.com>
2024-07-24 19:50:58 +0100

    Keep every promise you make

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      const response = {
+        status: 200,
+        body: 'Success',
+      };
+  
+      if (success) {
+        resolve(response);
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }
+  
