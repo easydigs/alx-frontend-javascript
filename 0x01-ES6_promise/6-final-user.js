a2577007c1534c982e2548951f3cc9271e222108
easydigs <easydigitalkoncept@gmail.com>
2024-07-25 11:42:14 +0100

    Task 6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,18 @@
 /* eslint-disable */
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(
   firstName,
   lastName,
-  filename
-) {
+  filename) {
   const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(filename),
-  ]).then((res) =>
-    res.map((o) => ({
-      status: o.status,
-      value: o.status === "fulfilled" ? o.value : String(o.reason),
-    }))
-  );
+  ]);
+
+  return results.map((o) => ({
+    status: o.status,
+    value: o.status === 'fulfilled' ? o.value : String(o.reason),
+  }));
 }
