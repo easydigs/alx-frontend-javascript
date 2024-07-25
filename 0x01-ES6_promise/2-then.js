1bf8b4cc01868b2fe895d0e46a652daf32d97611
easydigs <easydigitalkoncept@gmail.com>
2024-07-25 11:26:07 +0100

    Task 2. Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,6 @@
-/* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-    return promise 
+  return promise
     .then((response) => ({ status: 200, body: 'success' }))
-    .catch(()=> new Error())
-    .finally(()=> console.log('Got a response from the API'));
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
