9dff5d233608db74a15341032429ceaec02fd52f
easydigs <easydigitalkoncept@gmail.com>
2024-07-25 11:11:36 +0100

    Task 5. Reject the promises

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,4 @@
-/* eslint-disable */
 export default function uploadPhoto(filename) {
-    return Promise
-    .reject(new Error(`${filename} cannot be processed`));
+  return Promise
+  .reject(new Error(`${filename} cannot be processed`));
 }
