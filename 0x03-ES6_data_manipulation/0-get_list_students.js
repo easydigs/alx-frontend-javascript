97cf8960ce68315c4069d057899d4f1e28c12a65
easydigs <easydigitalkoncept@gmail.com>
2024-07-30 14:15:56 +0100

    0. Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,22 @@
-export function getListStudents() {
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
+/* eslint-disable */
+export default function getListStudents() {
+    const Student1 = {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    };
+  
+    const Student2 = {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    };
+  
+    const Student3 = {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    };
+  
+    return [Student1, Student2, Student3];
 }
