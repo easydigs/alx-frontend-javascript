6f5b075945214b60fd9eeb33018497b21d1ba9db
easydigs <easydigitalkoncept@gmail.com>
2024-07-30 18:04:44 +0100

    Task 10. update

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default function updateUniqueItems(map) {
     if (!(map instanceof Map)) {
-      throw new Error('Cannot process');
+        throw new Error('Cannot process');
     }
     map.forEach((value, key) => {
-      if (value === 1) {
-        map.set(key, 100);
-      }
+        if (value === 1) {
+            map.set(key, 100);
+        }
     });
 }
